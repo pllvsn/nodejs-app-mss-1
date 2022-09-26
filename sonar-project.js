804397e988186429c23b4066cdca613c6f4dc658
Pallav <35934299+pllvsn@users.noreply.github.com>
2022-09-26 14:40:18 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
   serverUrl: 'http://3.235.198.97:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectName': 'Node JS Application - Samples23',
 	    'sonar.login': 'sqa_7f45f11945901e98d45f7484ed8bf338b35e9196',
 	    'sonar.projectKey':'NodeJS',
 	    //'sonar.login': '9bcdd15299583c728cf53837f2ab21fcb36c5aa4',
